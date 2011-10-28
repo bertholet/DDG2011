87cc7700046b5cb08b67e989e915d0c1022fc02c
bertholet <peter_bertholet@hotmail.com>
2011-10-28 23:29:18 +0200

    test

--- main.cpp
+++ main.cpp
@@ -9,8 +9,10 @@ int main(int argc, char *argv[])
 	QApplication app(argc, argv);
 	MainWindow window;
 
+	int i = 1;
+	i++;
 /*	mesh * bunny= new mesh("C:/Users/bertholet/Dropbox/workspace/RA/objfiles/bunny5k.obj", tuple3f(1.f,0.f,0.f), 2);
-	bunny->normalize();
+	bunny->normalize();//
 	
 	TutteDemo demo;
 	demo.run_multiBorder(*bunny, TutteWeights::cotan_weights_divAvor);
